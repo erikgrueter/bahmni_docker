ba44b4896c16907c7d2524b4f8ee83f30d4816e2
Erik Grueter <erikgrueter@gmail.com>
2018-09-27 21:08:56 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+service mysqld start
+service postgresql-9.2 start
+service openmrs start
+service httpd start
+service bahmni-reports start
+service bahmni-lab start
+service openerp start
+service bahmni-erp-connect start
+service atomfeed-console start
+service pacs-integration start
